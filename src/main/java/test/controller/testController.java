536fd95cfdeddd9f937e2d49aeb0009aaec74790
zhangcl52 <zhangcl52@ziroom.com>
2016-06-12 11:37:56 +0800

    first commit

---
+++ src/main/java/test/controller/testController.java
@@ -0,0 +1,29 @@
+package test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by homelink on 2016/5/24.
+ */
+@Controller
+@RequestMapping(value = "/demoTest")
+public class testController {
+    @RequestMapping(value = "/test")
+    public String test(HttpServletRequest request, ModelAndView model) {
+        System.out.println("zhangcl");
+        return "register";
+    }
+
+    @RequestMapping(value = "/testAdd")
+    public String testAdd(HttpServletRequest request, ModelAndView model) {
+        String name = request.getParameter("name");
+        String pwd = request.getParameter("pwd");
+        System.out.println(name);
+        System.out.println(pwd);
+        return "register";
+    }
+}
---
+++ src/main/webapp/WEB-INF/test/register.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: homelink
+  Date: 2016/5/24
+  Time: 7:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>注册</title>
+    <script src="../js/jquery-1.11.1.js"></script>
+    <script src="../js/jquery.min-1.11.1.js"></script>
+</head>
+<body>
+    <form action="testAdd.do" >
+        姓名：<input name="name" value=""></input>
+        <br/>
+        密码：<input name="pwd"></input>
+        <br/>
+        <input type="submit" value="提交"></form>
+    </form>
+</body>
+
+</html>
