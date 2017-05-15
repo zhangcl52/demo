8503e01fd69b5b16199f019916f297aa1be2bd99
zhangcl <zhangchlo@sina.cn>
2017-05-15 08:13:01 +0800

    菜单管理中增加功能

---
+++ src/main/java/com/hunter/sys/dao/SysMenuDao.java
@@ -0,0 +1,1 @@
+package com.hunter.sys.dao;import com.hunter.base.dao.BaseDao;import com.hunter.sys.entity.SysMenu;import org.springframework.stereotype.Repository;/** * @Author zhangcl * @Date 17/5/14  下午11:38 */@Repositorypublic class SysMenuDao extends BaseDao<SysMenu> {}
